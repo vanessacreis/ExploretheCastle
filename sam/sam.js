6e7380a40729fadcbb0bc6db661efeaa73040d5a
vanessacreis <vanessacreis@live.com>
2021-11-19 14:39:57 -0300

    inclusao do js fase 1

---
+++ sam/sam.js
@@ -0,0 +1,11 @@
+function primeiraFase(){
+    var question = prompt ('O que fazer?\n\n1. Desaparecer discretamente pelos corredores em busca de um lugar vazio e aproveitar para explorar o estranho castelo.\n2. Fingir socializar e esperar pacientemente pela primeira oportunidade de ir para casa.')
+
+    if (question == 1){
+        window.location.href = "sam2.html"
+    }else if (question == 2){
+        window.location.href = "gameover1.html"
+    }else{
+        alert('Digite 1 ou 2')
+    }
+}
