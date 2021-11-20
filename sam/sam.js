18d41ebb40d117d772bfba18144bf9f01e9c94f6
vanessacreis <vanessacreis@live.com>
2021-11-20 16:51:30 -0300

    inclusão do javascript

--- sam/sam.js
+++ sam/sam.js
@@ -1,11 +1,21 @@
 function primeiraFase(){
-    var question = prompt ('O que fazer?\n\n1. Desaparecer discretamente pelos corredores em busca de um lugar vazio e aproveitar para explorar o estranho castelo.\n2. Fingir socializar e esperar pacientemente pela primeira oportunidade de ir para casa.')
 
-    if (question == 1){
-        window.location.href = "sam2.html"
-    }else if (question == 2){
-        window.location.href = "gameover1.html"
-    }else{
-        alert('Digite 1 ou 2 :]')
+    while (question != 1 || question != 2){
+        var question = prompt ('O que fazer?\n\n1. Desaparecer discretamente pelos corredores em busca de um lugar vazio e aproveitar para explorar o estranho castelo.\n2. Fingir socializar e esperar pacientemente pela primeira oportunidade de ir para casa.')
+        
+        if (question == 1){
+            window.location.href = "sam2.html"
+            break
+
+        }else if (question == 2){
+            window.location.href = "gameover1.html"
+            break
+            
+        }else{
+            alert('Digite 1 ou 2 :]')
+            break
+        }
+    
     }
+
 }
--- sam/sam2.js
+++ sam/sam2.js
@@ -1,11 +1,18 @@
 function segundaFase(){
-    var question = prompt ('O que fazer\n\n1. Arriscar passar pela janela e sair logo dali, mesmo que isso te garanta alguns arranhões.\n2. Seguir pelos corredores tranquilamente, afinal, adultos não tem medo de assombração.')
+    while (question != 1 || question != 2){
+        var question = prompt ('O que fazer\n\n1. Arriscar passar pela janela e sair logo dali, mesmo que isso te garanta alguns arranhões.\n2. Seguir pelos corredores tranquilamente, afinal, adultos não tem medo de assombração.')
 
-    if (question == 1){
-        window.location.href = 'gameover1.html'
-    }else if (question == 2){
-        window.location.href = 'sam3.html'
-    }else{
-        alert('Digite 1 ou 2 :]')
+        if (question == 1){
+            window.location.href = 'gameover2.html'
+            break
+        }else if (question == 2){
+            window.location.href = 'sam3.html'
+            break
+        }else{
+            alert('Digite 1 ou 2 :]')
+            break
+        }
+
     }
+    
 }
