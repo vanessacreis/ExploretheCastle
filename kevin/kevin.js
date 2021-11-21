03fe7098afb4be7c04f88c08088200007a3bdeda
vanessacreis <vanessacreis@live.com>
2021-11-21 16:11:54 -0300

    primeira versão páginas Kevin

---
+++ kevin/kevin.js
@@ -0,0 +1,16 @@
+function primeiraFaseKevin(){
+    while (question !=1 || question !=2){
+        var question = prompt ('O que fazer?\n\n1. Agradece dizendo que prefere ficar no salão, onde pode ser visto por todos como era sua intenção.\n2. Você aceita imediatamente, imaginando ser algo relacionado ao prêmio surpresa ou a sua brilhante atuação de pirata.')
+
+        if (question == 1){
+            window.location.href = "gameover1.html"
+            break
+        }else if (question == 2){
+            window.location.href = "kevin2.html"
+            break
+        }else{
+            alert('Digite 1 ou 2 ;D')
+            break
+        }
+    }
+}
